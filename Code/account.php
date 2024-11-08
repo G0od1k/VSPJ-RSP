a3fff0489822d1a53089bedf58a5bb4255ae57c1
YogurtViktosha <and.vik2005@gmail.com>
2024-11-08 23:33:09 +0100

    Add account page

---
+++ Code/account.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Účet</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include "./modules/header.php" ?>
+    <main>
+        <h1><?php echo $user["name"] ?></h1>
+        <p>Role:
+            <?php echo ["Čtenář", "Autor", "Redaktor", "Admin"][$user["role"]] ?>
+        </p>
+    </main>
+    <?php include "./modules/footer.php" ?>
+</body>
+
+</html>
