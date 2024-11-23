3a58dea2b33c6a8a2cb309c2578d6c26aa615dd5
G0od1k <g0cd1k@gmail.com>
2024-11-23 23:46:26 +0100

    Rename variable

--- Code/set_reviewer_act.php
+++ Code/set_reviewer_act.php
@@ -5,11 +5,11 @@ include "connect.php";
 include "user.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_SESSION['email'])) {
-    $article_id = $connect->real_escape_string($_POST['aid']);
+    $id = $connect->real_escape_string($_POST['aid']);
     $user_id = $connect->real_escape_string($_POST['uid']);
 
     $sql = "INSERT INTO ReviewBy (id_user, id_article) 
-                VALUES (" . $user_id . " , $article_id)";
+                VALUES (" . $user_id . " , $id)";
     $connect->query($sql);
 }
 
