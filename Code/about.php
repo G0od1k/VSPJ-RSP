7c87fe18cdffad3be385202ca04187c58855273e
YogurtViktosha <and.vik2005@gmail.com>
2024-11-24 14:11:15 +0100

    Add about page

---
+++ Code/about.php
@@ -0,0 +1,47 @@
+<?php include "./connect.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O nás</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php include "./modules/header.php" ?>
+
+    <main id="about">
+        <h1>O nás</h1>
+        <p>
+            Jsme mladé a ambiciózní nakladatelství, které vzniklo díky šesti studentům Vysoké školy polytechnické
+            Jihlava (VŠPJ) na začátku září roku 2024. Naším hlavním cílem je nejen úspěšně zvládnout předmět RSP, ale
+            také vytvořit platformu, která umožní autorům sdílet své příběhy a myšlenky s širokou veřejností.
+        </p>
+        <p>
+            <b>52 NEWS</b> představuje jedinečný odrazový můstek pro začínající autory, kteří se zaměřují na mladé
+            publikum –
+            studenty a mladé dospělé ve věku od 18 do 28 let. Snažíme se podporovat kreativitu, inovace a odvahu v
+            literárním světě.
+        </p>
+        <p>
+            Máme velké ambice: chceme rozvíjet náš projekt a vytvořit obdobu pro mnoho zemí a univerzit v Evropě. Naší
+            vizí je umožnit studentům svobodně číst texty, které je skutečně zajímají, a ne to, co diktují velká
+            nakladatelství propagující své vlastní agendy. Věříme v literaturu bez politických podtextů – v čistou,
+            autentickou tvorbu.<br />
+            Pokud jste autor, který hledá prostor pro publikaci svých textů, neváhejte nás kontaktovat. Rádi vám
+            pomůžeme dostat vaši tvorbu do světa!
+        </p>
+        <h2>Kontaktujte nás:</h2>
+        <p>
+            E-mail: <a href="mailto:hlavniredaktor@vspj.cz">hlavniredaktor@vspj.cz</a><br />
+            Společně vytváříme prostor, kde vaše slova mohou zazářit!
+        </p>
+
+    </main>
+
+    <?php include "./modules/footer.php" ?>
+</body>
+
+</html>
