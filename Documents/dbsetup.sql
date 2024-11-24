ea6ddd48315edac0c41206029fae388833717a8f
G0od1k <g0cd1k@gmail.com>
2024-11-24 11:40:53 +0100

    Update dbsetup.sql

--- Documents/dbsetup.sql
+++ Documents/dbsetup.sql
@@ -44,7 +44,6 @@ CREATE TABLE Review (
     id_article INT,
     text TEXT,
     datetime DATETIME,
-    PRIMARY KEY (id_user, id_article),
     FOREIGN KEY (id_user) REFERENCES User(id),
     FOREIGN KEY (id_article) REFERENCES Article(id)
 );
